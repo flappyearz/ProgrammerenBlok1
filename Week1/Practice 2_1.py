4375be5bc74c73c0cfda08fb289b0831cf26091d
flappyearz <tijnie75@gmail.com>
2017-09-13 11:14:33 +0200

    Project

--- Week1/Practice 2_1.py
+++ Week1/Practice 2_1.py
@@ -3,4 +3,5 @@ letter = ('A', 'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'C', 'C')
 a = letter.count('A')
 b = letter.count('B')
 c = letter.count('C')
-print(a, b, c)
+lijst = [a,b,c]
+print(lijst)
