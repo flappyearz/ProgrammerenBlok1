ec4a0988e4ccf0a331b5daabea1c623b90e89ca8
flappyearz <tijnie75@gmail.com>
2017-09-13 12:32:19 +0200

    Week 2 practice opdrachten

---
+++ Week2/Practice 3_1.py
@@ -0,0 +1,8 @@
+score = int(input("Geef je score: "))
+
+if score > 15:
+    print("Gefeliciteerd!")
+    print("Met een score van "+str(score)+ " ben je geslaagd!")
+
+else:
+    print("JAMMER DAN!")
